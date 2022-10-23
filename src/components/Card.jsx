8862c05fdb5900df41745cb8c97927e6ef02be26
Jörg Oertel <tyskerjoerg@gmail.com>
2022-10-23 17:49:00 +0200

    no changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,19 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 import "../styles/card.css";
 
-const Card = ({ card: { id, name, email } }) => (
-  <div className="card-container">
-    <img
-      alt="monster"
-      src={`https://robohash.org/${id}?set=set2&size=180x180`}
-    />
-    <h2>{name}</h2>
-    <p>{email}</p>
-  </div>
+const Card = ({card: {id, name, email}}) => (
+
+    <div className="card-container">
+        <img
+            alt="monster"
+            src={`https://robohash.org/${id}?set=set2&size=180x180`}
+        />
+        <h2>{name}</h2>
+        <p>{email}</p>
+    </div>
 );
 
 export default Card;
 
 Card.propTypes = {
-  card: PropTypes.object.isRequired,
+    card: PropTypes.object.isRequired,
 };
