1117d00c41b3b12d89dc8266b1269dd989042210
Jörg Oertel <tyskerjoerg@gmail.com>
2022-10-26 09:04:00 +0200

    clead up code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React, { useState, useEffect, useRef } from "react";
-import CardList from "./components/CardList";
-import SearchBox from "./components/SearchBox";
+
 import "./styles/App.css";
 
 function App() {
 
+    // https://jsonplaceholder.typicode.com/users
+    /*
+        <div className="card-container">
+        <img
+            alt="monster"
+            src={`https://robohash.org/${id}?set=set2&size=180x180`}
+        />
+        <h2>{name}</h2>
+        <p>{email}</p>
+    </div>
+     */
   return (
     <div className="App">
       <h1>Monster Rolodex</h1>
